893bf41b5a943d300d9d2a2766bafc89f445af5d
Andrew Ho <andrew@kamcord.com>
2017-02-17 01:46:44 -0800

    add folder structure

---
+++ frontend/src/layouts/CoreLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './CoreLayout.css'
+// import '../../styles/core.scss'
+
+export const CoreLayout = ({ children }) => (
+  <div className='core-layout'>
+    {children}
+  </div>
+)
+
+CoreLayout.propTypes = {
+  children : React.PropTypes.element.isRequired
+}
+
+export default CoreLayout
